5965c8ad51666024fad58272ac45299d7a7113ce
Ryo Fukumuro <rfkm@nop.sh>
2016-02-20 21:35:58 +0900

    Change deploy order

--- release.sh
+++ release.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
-cd cloverage
+cd lein-cloverage
 
 lein vcs assert-committed
 lein change version leiningen.release/bump-version release
-
-cd ../lein-cloverage
-
-lein change version leiningen.release/bump-version release
-lein sync-core-lib-version
-lein vcs commit
-lein vcs tag
-lein deploy
-lein change version leiningen.release/bump-version patch
 lein sync-core-lib-version
 
 cd ../cloverage
 
+lein change version leiningen.release/bump-version release
+lein vcs commit
+lein vcs tag
 lein deploy
 lein change version leiningen.release/bump-version patch
+
+cd ../lein-cloverage
+
+lein deploy
+lein change version leiningen.release/bump-version patch
+lein sync-core-lib-version
 lein vcs commit
